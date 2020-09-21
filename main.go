dbad57e50bbda2e4b6fea60dd1b88161505a724f
sunimalherath <sunimaldv@gmail.com>
2020-09-21 12:31:22 +1000

    adding link checking functionality

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	links := []string{
+		"http://apple.com",
+		"http://google.com",
+		"http://microsoft.com",
+		"http://golang.org",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "seems to be down")
+		return
+	}
+
+	fmt.Println(link, "is up and running!")
 }
