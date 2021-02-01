515611179911a9c26854b7d8fe6c80a36554b53f
sunimalherath <sunimaldv@gmail.com>
2021-02-01 23:11:51 +1100

    Adding comments

--- range-channel/main.go
+++ range-channel/main.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	c := make(chan int) // UNBUFFERED CHANNEL
 
+	// THIS ANONYMOUS FUNC EXECUTES IN A NEW THREAD. AND MAIN FUNC GOES TO LINE 17.
+	// AND WAITS THERE TILL IT GETS A VALUE FROM 'c'
 	go func() {
 		for i := 0; i < 10; i++ {
-			c <- i // KEEP ADDING VALUES TO THE CHANNEL
+			c <- i // PASS A VALUE AND WAITS UNTIL LINE 17 READY TO RECIVE NEXT VALUE.
 		}
 		close(c) // ONCE DONE, CLOSE THE CHANNEL
 	}()
