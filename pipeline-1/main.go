3448c208c91a8f36c07b268e1db4b06f6634c2fd
sunimalherath <sunimaldv@gmail.com>
2021-02-08 23:26:35 +1100

    implement pipeline

---
+++ pipeline-1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// SETUP THE PIPELINE
+	c := gen(2, 3, 4)
+	out := sq(c)
+	for n := range out {
+		fmt.Println(n)
+	}
+}
+
+func gen(nums ...int) chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+
+	return out
+}
