f635fda081ea617f3458d665c129a98f51a1665d
sunimalherath <sunimaldv@gmail.com>
2021-02-08 23:30:13 +1100

    refactor pipeline code

---
+++ pipeline-2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// SETUP THE PIPELINE
+	out := sq(gen(2, 3, 4))
+	for n := range out {
+		fmt.Println(n)
+	}
+}
+
+func gen(nums ...int) chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+
+	return out
+}
